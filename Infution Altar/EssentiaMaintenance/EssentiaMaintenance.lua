3bcd50fa92507769f9a80e22e025a0e77176bceb
XCrz <2114085439@qq.com>
2024-12-30 12:06:31 +0800

    Update

--- Infution Altar/EssentiaMaintenance/EssentiaMaintenance.lua
+++ Infution Altar/EssentiaMaintenance/EssentiaMaintenance.lua
@@ -10,12 +10,12 @@ local gpu = component.gpu
 local modem = component.modem
 local controller = component.me_controller
 
-local ErrList = {["无样板"]={},["未命名"]={},["无原料"]={}}
+local infoList = {["无样板"]={},["未命名"]={},["无原料"]={},["massage"]={}}
 local length,height = gpu.getViewport()
 local maxRows = floor((height - 6)/2)
 local maxLens = length - 2
 local craftingList = {}
-local ErrChange = true
+local infoChange = true
 
 local function getEssCraftable()
 	local filter,list = {name = "thaumicenergistics:crafting.aspect"},{}
@@ -25,23 +25,10 @@ local function getEssCraftable()
 	return list
 end
 
-local function getMissingEss()
-	local list = controller.getEssentiaInNetwork()
-	local allEss = {}
-    for _, v in pairs(list) do
-    	allEss[v.label:match("^%S+"):lower()] = true
-    end
-    for k,_ in pairs(listRequest) do
-    	if not allEss[k] then
-    		return k 
-    	end
-    end
-end
-
-local function changeErr(typeStr,nameStr,statusNum)
-	if ErrList[typeStr][nameStr] ~= statusNum then
-		ErrChange = true
-		ErrList[typeStr][nameStr] = statusNum
+local function changeErr(typeStr,nameStr,status)
+	if infoList[typeStr][nameStr] ~= status then
+		infoChange = true
+		infoList[typeStr][nameStr] = status
 	end
 end
 
@@ -57,7 +44,7 @@ local function getUsableCPU()
 		end
 	end
 	if not hasNamedCPU then
-		changeErr("未命名","cpu",1)
+		changeErr("未命名","cpu",true)
 	end
 end
 
@@ -92,6 +79,19 @@ local function requestEss(name,NUM)
 	end
 end
 
+local function requestMissingEss(allEss)
+	local list = {}
+    for _, v in pairs(allEss) do
+    	list[v.label:match("^%S+"):lower()] = true
+    end
+    for k,_ in pairs(listRequest) do
+    	if not list[k] then
+			requestEss(k,1)
+    		return nil
+    	end
+    end
+end
+
 local function showInfoInBounds(x,y,lens,rows,info)
 	local posX = x
 	for k,_ in pairs(info) do
@@ -107,28 +107,20 @@ local function showInfoInBounds(x,y,lens,rows,info)
 	end
 end
 
-local function showTheMassage()
-	gpu.fill(1,2,length,height," ")
-	gpu.set(length - 10,height - 3,"已发送报文")
-	gpu.set(length - 8,height - 1,tostring(massage))
-end
-
 local function DisplayInformation()
-	if massage == false then
-		if ErrChange then
-			showTheMassage()
-			if ErrList["未命名"]["cpu"] == 1 then
-				gpu.set(1,3," • ERROR: 缺少特殊名称的CPU，无法下单！")
-			end
-			gpu.set(1,5,"---以下源质缺少样板---")
-			showInfoInBounds(1,6,maxLens,maxRows,ErrList["无样板"])
-			gpu.set(1,6 + maxRows,"---以下源质缺少原料---")
-			showInfoInBounds(1,7 + maxRows,maxLens,maxRows,ErrList["无原料"])
+	if infoChange then
+		gpu.fill(1,2,length,height," ")
+		if infoList["未命名"]["cpu"] == true then
+			gpu.set(1,3," • ERROR: 缺少特殊名称的CPU，无法下单！")
 		end
-	else
-		showTheMassage()
+		gpu.set(1,5,"---以下源质缺少样板---")
+		showInfoInBounds(1,6,maxLens,maxRows,infoList["无样板"])
+		gpu.set(1,6 + maxRows,"---以下源质缺少原料---")
+		showInfoInBounds(1,7 + maxRows,maxLens,maxRows,infoList["无原料"])
+		gpu.set(length - 10,height - 3,"已发送报文")
+		gpu.set(length - 8,height - 1,tostring(massage))
 	end
-	ErrChange = false
+	infoChange= false
 end
 
 local function main()
@@ -136,20 +128,20 @@ local function main()
 	listRequest = getEssCraftable()
 	modem.setStrength(signalStrength)
 	modem.open(port)
-	
+
 	os.execute("cls")
 	while true do
 		os.sleep(1)
 		repeat
 			massage = true
-			if getMissingEss() then
-				requestEss(getMissingEss(),1)
-			end
-			for _, v in pairs(controller.getEssentiaInNetwork()) do
+			local currentEss = controller.getEssentiaInNetwork()
+			requestMissingEss(currentEss)
+			for _, v in pairs(currentEss) do
 				if v.amount < minReserve then
 					requestEss(v.label:match("^%S+"):lower(),Num_perRequest)
 				end
 			end
+			changeErr("massage","content",massage)
 			modem.broadcast(port,massage)
 			DisplayInformation()
 		until massage == true
