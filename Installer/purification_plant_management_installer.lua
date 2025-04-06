43904e69828a1b78968a5c149e961021c39ea900
XCrz <2114085439@qq.com>
2025-04-06 09:14:53 +0800

    fix

--- Installer/purification_plant_management_installer.lua
+++ Installer/purification_plant_management_installer.lua
@@ -13,7 +13,7 @@ local downloadList = {
     {"/core/craftables.lua", 43655},
     {"/core/core_logic.lua", 31008},
     {"/core/base/machines.lua", 9835},
-    {"/core/base/me_network.lua", 17278},
+    {"/core/base/me_network.lua", 52978},
     {"/core/base/preset_data.lua", 44286},
 }
 
